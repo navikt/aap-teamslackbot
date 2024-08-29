0c65847e28d8082aaad0fe05f824896b56587a1a
Tor Idland <tor.idland@nav.no>
2024-08-29 13:38:06 +0200

    socket mode

--- src/app.js
+++ src/app.js
@@ -6,8 +6,10 @@ const setupEventListener = require("./events");
 const PORT = process.env.PORT || 3000
 
 const app = new App({
+    socketMode: true,
     token: process.env.SLACK_BOT_TOKEN,
     signingSecret: process.env.SLACK_SIGNING_SECRET,
+    appToken: process.env.SLACK_APP_TOKEN,
     port: PORT,
     customRoutes: [
         {
