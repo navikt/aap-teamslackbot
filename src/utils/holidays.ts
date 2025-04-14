cd4dfad36bd7e29a01308d91262820b5d4eb3dfe
Tor Idland <tor.idland@nav.no>
2025-04-14 09:22:50 +0200

    legger til ferie i botten fra 17. til 21. april

--- src/utils/holidays.ts
+++ src/utils/holidays.ts
@@ -1,9 +1,11 @@
 export function isTodayAHoliday() {
     const today = new Date();
+    const påskeStart = new Date(today.getFullYear(), 4, 17);
+    const påskeSlutt = new Date(today.getFullYear(), 4, 21);
     const julStart = new Date(today.getFullYear(), 11, 21);
     const julSlutt = new Date(today.getFullYear(), 11, 31);
     const nyttaarStart = new Date(today.getFullYear(), 0, 1);
     const nyttaarSlutt = new Date(today.getFullYear(), 0, 3);
 
-    return ((today >= julStart && today <= julSlutt) || (today >= nyttaarStart && today <= nyttaarSlutt));
+    return ((today >= julStart && today <= julSlutt) || (today >= nyttaarStart && today <= nyttaarSlutt)|| (today >= påskeStart && today <= påskeSlutt));
 }
