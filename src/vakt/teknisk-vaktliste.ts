e88140ef9b58fe976fb46afefd7162be46f39059
HenrikBuggeHalvorsen <henrik.bugge.halvorsen@nav.no>
2025-05-08 12:45:37 +0200

    legger til nye utviklere i vaktlisten

--- src/vakt/teknisk-vaktliste.ts
+++ src/vakt/teknisk-vaktliste.ts
@@ -2,17 +2,21 @@ const utviklere = [
     'U08HFRGBJ9G',      // Marianne
     'UEHPV7NAE',        // Matias
     'U02KTLW1QLC',      // Øivind
-    'U0184NNTXL5' ,      // Peter
+    'U0184NNTXL5' ,     // Peter
     'U03HT22GCBU',      // Thomas
-    // 'U02EFCUUQF8',      // Tor
-    'U03LBKLNNAF', // Vetle
-    'U07946678MA', // Henrik Bugge
+    // 'U02EFCUUQF8',   // Tor
+    'U03LBKLNNAF',      // Vetle
+    'U07946678MA',      // Henrik Bugge
     'U076BSY04G3',      // Fredrik
     'U7SRD021L',        // Frode
     'U075X6VRNLF',      // Ine
     'U0115SZM8PQ',      // Charlie
     'U01CH1LE2FK',      // Nicolas
     'U08EGKCC8FM',      // Selma
+    'U08MD6XU9K6',      // Greger
+    'U02TAL2D5L2',      // Henrik Gundersen
+    'U64P58KM3',        // Steffen
+    'U60QWL9LN',        // Hein
 ]
 export function hentDagensTekniskeVakt() {
     var daysSinceEpoc = Math.floor(Date.now() / 24 / 60 / 60 / 1000)
