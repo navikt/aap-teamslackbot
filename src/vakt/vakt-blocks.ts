5537fc18da37a0b00beca33ba7db2a5628ef42a6
HenrikBuggeHalvorsen <henrik.bugge.halvorsen@nav.no>
2025-08-06 12:50:11 +0200

    tekstforandringer på funksjonell vakt

--- src/vakt/vakt-blocks.ts
+++ src/vakt/vakt-blocks.ts
@@ -3,7 +3,7 @@ export function vaktBlocks(dagensTekniskeVakt: string, dagensTestVakt: string) {
     const logLink = "<https://logs.adeo.no/app/discover#/view/71a530ed-9b1d-4c5f-bc75-9efcc262323a|Elastic - feil i prod>";
     const grafanaLink = "<https://grafana.nav.cloud.nais.io/goto/0k4EabAHg?orgId=1|Grafana - Error og Warning i prod>"
     const grafanaDashboardsLink = "<https://grafana.nav.cloud.nais.io/d/deflf77wcdptsd/oversikt-over-aap-dashboard?orgId=1&from=now-6h&to=now&timezone=browser|Grafana - Aap dashboards>"
-    const testVaktTekst = `:test_tube: *Dagens testvakt er <@${dagensTestVakt}>*`;
+    const testVaktTekst = `:test_tube: *Dagens funksjonelle vakt er <@${dagensTestVakt}>*`;
     return [
         {
             "type": "divider"
@@ -22,7 +22,7 @@ export function vaktBlocks(dagensTekniskeVakt: string, dagensTestVakt: string) {
             "type": "section",
             "text": {
                 "type": "mrkdwn",
-                "text": `${testVaktTekst} \nHovedansvar for å følge opp testerene i Teams-kanalen`
+                "text": `${testVaktTekst}`
             }
         },
     ]
