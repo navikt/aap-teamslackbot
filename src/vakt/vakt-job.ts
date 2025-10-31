1f33363f4f49227c00a36f71b50d52f9e8c6ae7c
Tor Idland <tor.idland@nav.no>
2025-10-31 13:53:25 +0100

    glemte å starte job

--- src/vakt/vakt-job.ts
+++ src/vakt/vakt-job.ts
@@ -89,7 +89,9 @@ export function updateVaktUserGroup(app: App) {
       users: 'U02EFCUUQF8',
     });
   }
-  const time = '50 13 * * 1-5' // kl 11:11:11, man-fre, alle uker, alle måneder
+  const time = '56 13 * * 1-5' // kl 11:11:11, man-fre, alle uker, alle måneder
   const job = new CronJob(time, onTick, null, false, TIMEZONE)
+
+  job.start()
 
 }
