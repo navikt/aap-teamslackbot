b450ae195891423fa7e7c465e149a6b1eb86bfb7
VHollund <vetle.hollund@nav.no>
2026-01-05 09:25:15 +0100

    vetle er tilbake på jobb

--- src/workplace/workplace-blocks.ts
+++ src/workplace/workplace-blocks.ts
@@ -7,7 +7,7 @@ export type WorkplaceBlock = {
 } | { type: 'divider'}
 /*
 
-            "text": ":zoom-icon: Hjemme / Zoom \`2\`\n <@U03LBKLNNAF>, <@U07946678MA>"
+            "text": ":zoom-icon: Hjemme / Zoom \`1\`\n <@U03LBKLNNAF>, <@U07946678MA>"
  */
 const blocks: WorkplaceBlock[] = [
     {
@@ -17,7 +17,7 @@ const blocks: WorkplaceBlock[] = [
         "type": "section",
         "text": {
             "type": "mrkdwn",
-            "text": ":zoom-icon: Hjemme / Zoom \`2\`\n "
+            "text": ":zoom-icon: Hjemme / Zoom \`1\`\n <@U03LBKLNNAF>"
         },
         "accessory": {
             "type": "button",
